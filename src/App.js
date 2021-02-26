ad2cc1e6568e390f25e53b027b3e6edb703417af
Md. Mojnu Miah <mojnu.cse.pust@gmail.com>
2021-02-26 08:58:01 +0600

    end module 31

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
   const nayok = ['Arefin Shuvo', 'Bapparaz', 'Manna', 'Dipjol', 'Mojnu']
-  const player = ['Mojnu', 'Ibrahim', 'Ariful'];
+  const persons = ['Mojnu', 'Ibrahim', 'Ariful'];
   const products = [
-    {productsName: 'Photoshop', mrp: '$52'},
-    {productsName: 'Illustrator', mrp: '$20'},
-    {productsName: 'MS Word', mrp: '$10'}
+    { productsName: 'Photoshop', mrp: '$52' },
+    { productsName: 'Illustrator', mrp: '$20' },
+    { productsName: 'MS Word', mrp: '$10' }
   ];
   const profession = ['Web Developer', 'High School Teacher', 'Manager']
   return (
     <div className="App">
       <header className="App-header">
         <p>I am a react person</p>
+        <Users></Users>
+        <Counter></Counter>
         <ul>
           {
             nayok.map(newNayok => <li>{newNayok}</li>)
@@ -22,13 +25,16 @@ function App() {
           }
         </ul>
         {
-          products.map(addProduct => <Product product = {addProduct}></Product>)
+          products.map(addProduct => <Product product={addProduct}></Product>)
+        }
+        {
+          persons.map(newPerson => <Person person={newPerson}></Person>)
         }
         {/* <Product product = {products[0]}></Product>
         <Product product = {products[1]}></Product> */}
-        <Person name={player[0]} profession={profession[0]}></Person>
+        {/* <Person name={player[0]} profession={profession[0]}></Person>
         <Person name={player[1]} profession={profession[1]}></Person>
-        <Person name={player[2]} profession={profession[2]}></Person>
+        <Person name={player[2]} profession={profession[2]}></Person> */}
       </header>
     </div>
   );
@@ -43,13 +49,13 @@ function Person(props) {
   return (
     <div>
       <div style={stylePerson}>
-        <h2>My Name: {props.name}</h2>
-        <h2>Profession: {props.profession}</h2>
+        <h2>My Name: </h2>
+        <h2>Profession: </h2>
       </div>
     </div>
   )
 }
-function Product(props){
+function Product(props) {
   const styleProduct = {
     border: '1px solid gray',
     color: 'black',
@@ -59,12 +65,42 @@ function Product(props){
     margin: '10px',
     float: 'left'
   }
-  const {productsName, mrp} = props.product;
+  const { productsName, mrp } = props.product;
   return (
     <div style={styleProduct}>
-        <h2>{productsName}</h2>
-        <h1>{mrp}</h1>
-        <button>Buy Now</button>
+      <h2>{productsName}</h2>
+      <h1>{mrp}</h1>
+      <button>Buy Now</button>
+    </div>
+  )
+}
+function Counter() {
+  const [count, setCount] = useState(0);
+  const handleIncrease = () => setCount(count + 1);
+  //const handleDecrease = () => setCount(count - 1);
+  return (
+    <div>
+      <h1>Counter: {count}</h1>
+      <button onMouseMove = {() => setCount(count - 1)}>-</button>
+      <button onClick={handleIncrease}>+</button>
+    </div>
+  )
+}
+function Users(){
+  const [users, setUsers] = useState([]);
+  useEffect(() =>{
+    fetch('https://jsonplaceholder.typicode.com/users/')
+    .then(res => res.json())
+    .then(data => setUsers(data));
+  }, [])
+  return (
+    <div>
+      <h3>Dynamic User: {users.length}</h3>
+    <ul>
+      {
+        users.map(user => <li>{user.name}</li>)
+      }
+    </ul>
     </div>
   )
 }
